deef74637149d57eec76335cc5be8ecf42ca964f
Katherine Lee <kate.lee168@gmail.com>
2015-03-17 20:28:12 -0400

    reverting back to index routing to folder, has css

--- web.go
+++ web.go
@@ -4,22 +4,15 @@ import (
        "fmt"
        "net/http"
        "os"
-       "html/template"
 )
 
 func main() {
-     http.HandleFunc("/", indexHandler)
-     http.Handle("/static/stylesheets/", http.StripPrefix("/static/stylesheets/", http.FileServer(http.Dir("/static/stylesheets"))))
+     fs := http.FileServer(http.Dir("static"))
+     http.Handle("/", fs)
 
      fmt.Println("listening...")
      err := http.ListenAndServe(":"+os.Getenv("PORT"),nil)
      if err != nil {
      	panic(err)
      }
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-     index := template.Must(template.ParseFiles(
-     	   "/static/example.html",))
-     index.Execute(w,nil)
 }
