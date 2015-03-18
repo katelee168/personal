545b049a81f99d3752aa030b83353ff8e2f3ac80
Katherine Lee <kate.lee168@gmail.com>
2015-03-17 20:25:11 -0400

    removed hardcoded filepath, doesn't play with heroku, trying to see if this works

--- web.go
+++ web.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
      http.HandleFunc("/", indexHandler)
-     http.Handle("/Users/Katherine/Desktop/Projects/go/src/personal/static/stylesheets/", http.StripPrefix("/static/stylesheets/", http.FileServer(http.Dir("/Users/Katherine/Desktop/Projects/go/src/personal/static/stylesheets"))))
+     http.Handle("/static/stylesheets/", http.StripPrefix("/static/stylesheets/", http.FileServer(http.Dir("/static/stylesheets"))))
 
      fmt.Println("listening...")
      err := http.ListenAndServe(":"+os.Getenv("PORT"),nil)
@@ -20,6 +20,6 @@ func main() {
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
      index := template.Must(template.ParseFiles(
-     	   "/Users/Katherine/Desktop/Projects/go/src/personal/static/example.html",))
+     	   "/static/example.html",))
      index.Execute(w,nil)
 }
